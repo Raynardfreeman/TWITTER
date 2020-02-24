2d53fabb25c5a3771179387c23c0da1466484828
Raynard Freeman <lilray220@icloud.com>
2020-02-24 15:31:07 -0500

    Initial Commit

---
+++ TWITTER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TWITTER
+//
+//  Created by Raynard Freeman II on 2/24/20.
+//  Copyright © 2020 Raynard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
